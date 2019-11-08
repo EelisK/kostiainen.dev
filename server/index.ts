11adf370ac0825141cc59845376036f92865e8ed
Eelis Kostiainen <eelis.kostiainen@aalto.fi>
2019-11-08 18:00:39 +0200

    Add logging

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,13 @@
 import app from "./src/app";
+import logger from "./src/logger";
 
-const server = app.listen(process.env.PORT);
+const PORT = process.env.PORT || 8080;
+
+const server = app.listen();
+logger.info(`App starting in ${PORT}`);
+
 const terminator = (code = 0) => async () => {
+  logger.info(`Terminating process ${code}`);
   await new Promise((resolve, reject) =>
     server.close(err => {
       if (err) reject(err);
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,7 @@
-import koa = require("koa");
+import * as koa from "koa";
+import * as loggerMiddleware from "koa-logger";
 
 const app = new koa();
+app.use(loggerMiddleware());
 
 export default app;
