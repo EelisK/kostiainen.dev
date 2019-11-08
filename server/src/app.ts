1643417772167b618f4d17cac7855ccc453fcac1
Eelis Kostiainen <eelis.kostiainen@aalto.fi>
2019-11-08 19:57:52 +0200

    Configure staticfiles

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,9 @@
 import * as koa from "koa";
 import * as loggerMiddleware from "koa-logger";
+import * as serve from "koa-static";
 
 const app = new koa();
 app.use(loggerMiddleware());
+app.use(serve(process.env.STATICFILE_DIRECTORY));
 
 export default app;
