b0fb434de83d902681c1386205298c0ed1cb283d
Eelis Kostiainen <eelis.kostiainen@aalto.fi>
2019-11-17 23:18:28 +0200

    Initialize redux

---
+++ client/src/components/App.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+export default class extends React.PureComponent {
+  render = () => <h1>Hello world!</h1>;
+}
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,4 +1,12 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import App from "./components/App";
+import { Provider } from "react-redux";
+import store from "./config/store";
 
-ReactDOM.render(<h1>Hello world!</h1>, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
