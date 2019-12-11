17e0fad666e7f8aa2f30a88d7efb06bac0680bdc
Eelis Kostiainen <eelis.kostiainen@aalto.fi>
2019-12-11 20:47:18 +0200

    Add error handling middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,17 @@
 import * as dotenv from "dotenv";
 import * as koa from "koa";
 import * as loggerMiddleware from "koa-logger";
+import * as bodyParser from "koa-bodyparser";
 import * as serve from "koa-static";
+
+import error from "./middlewares/error";
 
 dotenv.config();
 
 const app = new koa();
+app.use(bodyParser());
 app.use(loggerMiddleware());
+app.use(error);
 app.use(serve(process.env.STATICFILE_DIRECTORY));
 
 export default app;
