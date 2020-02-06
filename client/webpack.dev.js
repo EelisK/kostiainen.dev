3830c3ae50f6d73b12ca804e362687585c297c61
Eelis Kostiainen <eelis.kostiainen@aalto.fi>
2020-02-06 19:07:24 +0200

    Create proxy for connecting dev client and server

--- client/webpack.dev.js
+++ client/webpack.dev.js
@@ -1,4 +1,15 @@
 const merge = require("webpack-merge");
 const base = require("./webpack.base");
 
-module.exports = merge(base, { mode: "development" });
+module.exports = merge(base, {
+  mode: "development",
+  devServer: {
+    port: 3000,
+    proxy: {
+      "/**": {
+        target: "http://localhost:8080",
+        secure: false
+      }
+    }
+  }
+});
