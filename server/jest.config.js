4e46ce8d570a940849f949eb21442bf2c30f2656
Eelis Kostiainen <eelis.kostiainen@aalto.fi>
2020-02-08 12:11:44 +0200

    Update test setup

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   roots: ["<rootDir>/test"],
   testMatch: ["**/?(*.)+(spec|test).+(ts|tsx|js)"],
-  transform: { "^.+\\.(ts|tsx)$": "ts-jest" }
+  transform: { "^.+\\.(ts|tsx)$": "ts-jest" },
+  setupFiles: ["./jest.setup.js"],
+  setupFilesAfterEnv: ["./test/setup.ts"]
 };
