9d3e7977e7e5673dcef1a5a9fa95b2fbd6713937
Eelis Kostiainen <eelis.kostiainen@aalto.fi>
2020-02-08 15:17:19 +0200

    Add encrypted production keys

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,11 +3,15 @@ import * as koa from "koa";
 import * as loggerMiddleware from "koa-logger";
 import * as bodyParser from "koa-bodyparser";
 import * as serve from "koa-static";
+import * as path from "path";
 
 import error from "./middlewares/error";
 import contactRoutes from "./routes/contact";
 
 dotenv.config();
+
+if (process.env.NODE_ENV === "production")
+  dotenv.config({ path: path.join(__dirname, "../.env.prod") });
 
 const app = new koa();
 app.use(bodyParser());
